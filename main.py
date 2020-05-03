495ebc9da702d51e690f91c7ef266d5f9be41743
Jay Shridharani <jshridha@gmail.com>
2020-05-03 17:51:44 -0400

    chore: Add time to log messages

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import time
 import os
 
 import logging
+
 logger = logging.getLogger("inkbird")
 logger.setLevel(logging.INFO)
 
-logging.basicConfig()
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+)
 
 if __name__ == "__main__":
     address = os.environ.get("INKBIRD_ADDRESS")
